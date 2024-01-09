11fd969ea3e8582b6b3ea6f015acdce86c741517
rafazf <gerardo1022@hotmail.com>
2024-01-09 18:29:29 -0500

    eleccionContenedor

--- data.js
+++ data.js
@@ -2,38 +2,76 @@ const tablero = document.querySelector('.tablero')
 const objeto = document.querySelector('.objeto')
 const contenedor = document.querySelector('.item')
 const puntaje = document.querySelector('.puntaje')
+const botones = document.querySelector('.btonContent')
+const containerSeleccion = document.querySelector('.containerSeleccion')
 
 const vX = document.documentElement.clientWidth
 const vY = document.documentElement.clientHeight
 const i = 30;
+var score = 0;
 
-async function mover(obj, time) {
+//Guardar datos del juego en localStorage
+function saveDataGame(isColor){
+    localStorage.setItem('colorContenedor',isColor)
+}
+//obtener datos del juego del localStorage
+function getDataGame(){
+    let isColor = localStorage.getItem("colorContenedor") || false
+    return isColor
+}
+//indicar seleccion de boton del color
+async function  seleccionColor(e){
+    e.preventDefault()
+    if(e.target.matches(".bton")){
+        const getColor = e.target.getAttribute('data-color')
+        e.currentTarget.style.background = getColor     
+        await new Promise((resolve)=>{
+            setTimeout(resolve,1000)
+        })
+        saveDataGame(getColor)
+        containerSeleccion.remove()
+        tablero.classList.remove('hide')
+        //Iniciamos el juego
+        start()
+    }
+}
+botones.addEventListener('click',seleccionColor)
+
+async function mover(obj) {
     obj.style.left = posicionA()+'px'
-    contenedor.style.left = posicionA()+'px'
     const tiempoInicio = new Date().getTime();
-
+        
     function animar(){
-        const tiempoActual = new Date().getTime() + time;
+        const tiempoActual = new Date().getTime();
         const tiempoTranscurrido = tiempoActual - tiempoInicio;
 
         var nuevaPos = (tiempoTranscurrido/1000)*100
         obj.style.top = nuevaPos+'px';
         
-
         var poxy = posicion(obj)
         var poxyContenedor = posicion(contenedor)
         
         if(!haColisionado(poxy,poxyContenedor)){
-            requestAnimationFrame(animar)
+            if(afuera(obj)){
+                eliminarObjetos(obj)
+            }else{
+                requestAnimationFrame(animar)
+            }
         }else{
+            var colorObj = isColor(obj)
+            if(colorObj == contenedor.style.backgroundColor){
+                asignarPuntaje()
+            }
             eliminarObjetos(obj)
-            asignarPuntaje()
         }
     }
     requestAnimationFrame(animar)
 }
 function asignarPuntaje(){
-    puntaje.innerHTML+='🌟'
+    score = score + 1;
+    console.log(score)
+    //puntaje.textContent=score
+    puntaje.textContent+='🌟'
 }
 function eliminarObjetos(obj){
     obj.remove()
@@ -45,6 +83,11 @@ function haColisionado(objA, objB) {
         objA.y < objB.y + objB.height &&
         objA.y + objA.height > objB.y
     );
+}
+//funcion indica si un elemento esta fuera de los limites inferiores de la ventana
+function afuera(elemento) {
+    return elemento.offsetTop > window.innerHeight ||  elemento.offsetTop +
+    elemento.offsetHeight <= 0 ;
 }
 function posicion(obj){
     var coordenadas = obj.getBoundingClientRect();
@@ -76,15 +119,59 @@ document.addEventListener('keydown',function(e){
             break;
     }
 })
-//funcion para crear mas objetos
-
+//funcion para crear objeto
 function createNewObject(color){
     var newObject = document.createElement('div')
     newObject.classList.add("objeto")
     newObject.style.backgroundColor=color
     return tablero.appendChild(newObject)
 }
+//funcion que crea una cantidad n de objetos
+function createNewObjects(n){
+    return new Promise((resolve)=>{
+        async function generaObjetos(){
+            let count = 1;
+            async function creaYmueve(){
+                const newObject = createNewObject(getRandomColor())
+                mover(newObject)
+                await new Promise((resolve)=> setTimeout(resolve, 1000))
+            }
+            async function bucleObjetos(){
+                while(count < n){
+                    await creaYmueve();
+                    count++;
+                }
+            }
+            await bucleObjetos()
+            resolve('limite alcanzado'+n)
+        }
+        generaObjetos()
+    })
+}
+//funcion para generar colores aleatorios
+function getRandomColor() {
+    const arrColor = ['gray','brown','green','yellow','blue','red']
+    const numeroDecimal = Math.random();
+    const numeroAleatorio = Math.floor(numeroDecimal * 6) + 1;
+    return arrColor[numeroAleatorio];
+}
+//funcion identifica color de elemento 
+function isColor(obj){
+    const color = obj.style.backgroundColor
+    return color
+}
+//funcion para asignar el color del contenedor
+function setContainerColor(container, color){
+    container.style.backgroundColor = color
+}
+function start(){
+    contenedor.style.backgroundColor = getDataGame  ('colorContenedor')
+    var colorContenedor = contenedor.style.backgroundColor
 
-mover(objeto,100)
-mover(createNewObject('blue'),1000)
-mover(createNewObject('red'),10)
+    createNewObjects(10).then((mensaje)=>{console.log(mensaje)})
+}
+
+
+//mover(objeto,100)
+// mover(createNewObject('blue'),1000)
+// mover(createNewObject('red'),10)
